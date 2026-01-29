10c37eeb48b8bb0ff3f4750194768978e1d144b7
Lucy Pike <lucypike@Lucys-Computer.local>
2026-01-29 12:30:10 -0500

    comments at top

--- HW3.py
+++ HW3.py
@@ -1,6 +1,6 @@
-# Name:
-# Student ID:
-# Email:
+# Name: Lucy 
+# Student ID: 01323721
+# Email: lucypike@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
